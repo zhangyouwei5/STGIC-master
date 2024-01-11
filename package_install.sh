db1ba40a221af92c9534792194d4fcbcb93ee9a2
zhangyouwei5 <149301491+zhangyouwei5@users.noreply.github.com>
2024-01-11 22:35:12 +0800

    Add files via upload

---
+++ package_install.sh
@@ -0,0 +1,10 @@
+conda create -n test1 python=3.8.13
+conda install pytorch==1.13.0 torchvision==0.14.0 torchaudio==0.13.0 pytorch-cuda=11.7 -c pytorch -c nvidia
+pip install pandas==1.2.1
+conda install scikit-learn=0.23.2
+pip install matplotlib==3.6
+pip install scanpy==1.9.1
+pip install numba==0.56.0
+pip install numpy==1.20.1
+pip install scikit-misc==0.1.4
+pip install scipy==1.6.2
